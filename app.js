ba7ac1bbeaeda0b6b080c44e6e739d87bbbd9e28
Niraj Kafle <kafleniraj@gmail.com>
2021-05-09 14:42:31 +0545

    File add to db

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-
+const path = require("path");
+const crypto = require("crypto");
+const multer = require("multer");
+const GridFsStorage = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
+const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -14,11 +19,18 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(methodOverride("_method"));
+
+const mongoURI =
+  "mongodb+srv://niraj:niraj@cluster0.pmvbf.mongodb.net/healthDB?retryWrites=true&w=majority";
+const conn = mongoose.createConnection(mongoURI);
+
 //Connecting mongoose with mongodbb
-mongoose.connect("mongodb://localhost:27017/healthDB", {
+mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
@@ -33,23 +45,101 @@ const userSchema = new mongoose.Schema({
 });
 
 //Creating moodel for mogodb
-const User = mongoose.model("User", userSchema);
+const Upload = mongoose.model("Upload", userSchema);
+
+let gfs;
+
+conn.once("open", () => {
+  //Init stream
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection("uploads");
+});
+
+//Create storage engine
+const storage = new GridFsStorage({
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString("hex") + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: "uploads",
+        };
+        resolve(fileInfo);
+      });
+    });
+  },
+});
+const upload = multer({ storage });
 
 app.get("/", function (req, res) {
   res.render("index");
 });
 
-app.post("/", function (req, res) {
-  let newDetails = new User({
+app.post("/upload", upload.single("file"), function (req, res) {
+  let newDetails = new Upload({
     name: req.body.name,
     age: req.body.age,
     address: req.body.address,
     phone: req.body.phone,
     email: req.body.email,
+    file: req.body.file,
   });
   newDetails.save();
 
   res.render("index");
+});
+
+//Display all files in JSON
+app.get("/files", function (req, res) {
+  gfs.files.find().toArray((error, files) => {
+    if (!files || files.length === 0) {
+      return res.status(404).json({
+        error: "No files Exist",
+      });
+    }
+
+    return res.json(files);
+  });
+});
+
+//Display one file in JSON
+app.get("/files/:filename", function (req, res) {
+  gfs.files.findOne({ filename: req.params.filename }, (error, file) => {
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        error: "No files Exists",
+      });
+    }
+
+    return res.json(file);
+  });
+});
+
+//Display all files in JSON
+app.get("/image/:filename", function (req, res) {
+  gfs.files.findOne({ filename: req.params.filename }, (error, file) => {
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        error: "No files Exists",
+      });
+    }
+
+    // return res.json(file);
+
+    if (file.contentType === "image/jpeg" || file.contentType === "image/png") {
+      var readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        error: "Not an Image",
+      });
+    }
+  });
 });
 
 app.listen(3000, function (req, res) {
